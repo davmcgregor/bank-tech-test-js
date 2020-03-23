0fe4be30dc995230e7e041b5e8155eb8b04169a5
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-03-23 14:40:24 +0000

    Test acceptance criteria

--- spec/AccountSpec.js
+++ spec/AccountSpec.js
@@ -65,4 +65,12 @@ describe('Account', function() {
       expect(account.print_statement()).toEqual("date || credit || debit || balance\n03/01/2020 || || 1.00 || 1.00\n02/01/2020 || || 1.00 || 2.00\n01/01/2020 || 3.00 || || 3.00");
     });
   });
-})
+
+  it('should take in acceptance criteria', function() {
+    account.deposit(1000, "10/01/2012");
+    account.deposit(2000, "13/01/2012");
+    account.withdraw(500, "14/01/2012");
+    expect(account.print_statement()).toEqual("date || credit || debit || balance\n14/01/2012 || || 500.00 || 2500.00\n13/01/2012 || 2000.00 || || 3000.00\n10/01/2012 || 1000.00 || || 1000.00");
+  });
+
+});
