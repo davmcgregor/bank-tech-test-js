737603ead4c148d9653a54ddfe140f897dd41dae
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-03-23 12:59:57 +0000

    Add deposit feature

--- spec/AccountSpec.js
+++ spec/AccountSpec.js
@@ -14,4 +14,15 @@ describe('Account', function() {
   it('should instantiate with a balance of 0', function() {
     expect(account.balance).toBe(0);
   })
+
+  it('should instantiate with an empty statement array', function() {
+    expect(account.statement).toEqual([]);
+  })
+
+  describe('#deposit', function() {
+    it('should increase the account balance', function() {
+      account.deposit(1);
+      expect(account.balance).toEqual(1);
+    });
+  });
 })
--- src/Account.js
+++ src/Account.js
@@ -1,6 +1,14 @@
 (function(exports) {
-  function Account(balance = 0) {
+  function Account(balance = 0, statement = []) {
     this.balance = balance;
+    this.statement = statement;
   }
+
+  Account.prototype = {
+    deposit: function(amount){
+      this.balance += amount;
+    }
+  }
+
   exports.Account = Account;
 })(this);
