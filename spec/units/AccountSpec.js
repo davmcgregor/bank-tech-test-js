941a3c6e4ece2e757ba711b5038d4af74c4e7958
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-03-24 10:45:28 +0000

    Pass tests for master account object

--- spec/units/AccountSpec.js
+++ spec/units/AccountSpec.js
@@ -1,70 +1,69 @@
 'use strict';
 
 describe('Account', function() {
-  var account;
-  var date;
+  var account
+  var statement
 
   beforeEach(function() {
-    account = new Account();
-    date       = "23/03/2020";
+    statement = new Statement();
+    account = new Account(0, statement);
   });
 
   it('should be a defined object', function() {
     expect(account instanceof(Account)).toBe(true);
-  })
+  });
 
+  it('should have a balance property', function() {
+    expect(account.balance).toBeDefined();
+  });
+  
   it('should instantiate with a balance of 0', function() {
     expect(account.balance).toBe(0);
   })
 
-  it('should instantiate with an empty statement array', function() {
-    expect(account.statement).toEqual([]);
-  })
+  it('should have a statement property', function() {
+    expect(account.statement).toBeDefined();
+  });
+
+  it('should instantiate with the correct statement property', function() {
+    expect(account.statement).toBe(statement);
+  });
+
 
   describe('#print_statement', function() {
-    it('prints the account statement', function() {
-      expect(account.print_statement()).toEqual("date      || credit  || debit   || balance");
+
+    it('should return a string', function() {
+      expect(typeof account.print_statement()).toBe('string');
     });
   });
 
   describe('#deposit', function() {
+    
     it('should increase the account balance', function() {
       account.deposit(1);
       expect(account.balance).toEqual(1);
     });
 
     it('should make an entry on the account statement', function() {
+      spyOn(statement, 'addTransaction');
       account.deposit(1);
-      expect(account.print_statement()).toEqual("date      || credit  || debit   || balance\n23/03/2020 || 1.00 ||         || 1.00");
+      expect(account.statement.addTransaction).toHaveBeenCalled();
     });
 
-    it('should increase the account balance over multiple transations', function() {
-      account.deposit(1);
-      account.deposit(1);
-      expect(account.print_statement()).toEqual("date      || credit  || debit   || balance\n23/03/2020 || 1.00 ||         || 2.00\n23/03/2020 || 1.00 ||         || 1.00");
-    });
   });
 
-  describe('#withdrawal', function() {
-    it('should decrease the account balance', function() {
-      account.deposit(1);
-      account.deposit(1);
+  describe('#withdraw', function() {
+    
+    it('should decease the account balance', function() {
+      account.deposit(2);
       account.withdraw(1);
       expect(account.balance).toEqual(1);
     });
 
     it('should make an entry on the account statement', function() {
-      account.deposit(1);
-      account.deposit(1);
+      spyOn(statement, 'addTransaction');
       account.withdraw(1);
-      expect(account.print_statement()).toEqual("date      || credit  || debit   || balance\n23/03/2020 ||         || 1.00 || 1.00\n23/03/2020 || 1.00 ||         || 2.00\n23/03/2020 || 1.00 ||         || 1.00");
-    });
-
-    it('should decrease the account balance over multiple transations', function() {
-      account.deposit(3);
-      account.withdraw(1);
-      account.withdraw(1);
-      expect(account.print_statement()).toEqual("date      || credit  || debit   || balance\n23/03/2020 ||         || 1.00 || 1.00\n23/03/2020 ||         || 1.00 || 2.00\n23/03/2020 || 3.00 ||         || 3.00");
+      expect(account.statement.addTransaction).toHaveBeenCalled();
     });
   });
 });
